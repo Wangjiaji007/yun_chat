88d2e23b54fbf13b2b86257286a9707ab0d48a96
wangjiaji <wangjiaji52125@qq.com>
2015-07-07 15:06:27 +0800

    change authenticate user in application controller

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,5 +1,6 @@
 class WechatsController < ApplicationController
-  wechat_responder appid: "wx719462276ddb7614", secret: "8bfd196eed08a92f2112ef6d909180dd", token: "wangjiaji", access_token: Rails.root.join("tmp/access_token1")
+  wechat_responder appid: "wx719462276ddb7614", secret: "8bfd196eed08a92f2112ef6d909180dd", token: "wangjiaji",
+                   access_token: Rails.root.join("tmp/access_token1")
 
   on :text, with:"help", respond: "help content"
 end
