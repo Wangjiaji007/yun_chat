edd53d4bdcfb76e999faf18dffdee8367d68d77c
wangjiaji <wangjiaji52125@qq.com>
2015-07-07 13:20:10 +0800

    test wechat gem

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,6 +1,5 @@
 class WechatsController < ApplicationController
-  wechat_responder appid: "app1", secret: "secret1", token: "token1", access_token: Rails.root.join("tmp/access_token1")
-
+  wechat_responder appid: "wx719462276ddb7614", secret: "8bfd196eed08a92f2112ef6d909180dd", token: "wangjiaji", access_token: Rails.root.join("tmp/access_token1")
 
   on :text, with:"help", respond: "help content"
 end
